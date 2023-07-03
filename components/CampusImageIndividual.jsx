1ba06835f22a7f70850c1c72475239ddf5e1c6e9
Sudeep-K <pong32btl@gmail.com>
2023-07-03 17:34:25 +0545

    added image to gallery

--- components/CampusImageIndividual.jsx
+++ components/CampusImageIndividual.jsx
@@ -1,10 +1,12 @@
 "use client"
 import React from 'react'
+import Image from 'next/image'
+import CoverImage from '@/assets/OurDepartmentsGridBackground.png'
 
 const CampusImageIndividual = ({ content }) => {
   return (
-    <div styles={{display: "flex", backgroundColor: "grey", maxWidth: "100%", height: "100%"}}>
-        <img src={content.image_src} styles={{width: "100%", height: "auto"}}/>
+    <div style={{position: "relative"}}>
+        <Image src={CoverImage} layout="fill" objectFit="cover" alt="Campus Chief" />
     </div>
   )
 }
