4e23b940c44bb54b9dd805f54bf0774a1c806c7c
Sudeep-K <pong32btl@gmail.com>
2023-07-03 11:49:57 +0545

    adds testimonial, department and gallery component

--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
 "use client"
 import AboutThapathaliComponent from "@/components/AboutThapathaliComponent";
 import CampusChiefComponent from "@/components/CampusChiefComponent";
+import CampusImageGallery from "@/components/CampusImageGallery";
 import HeroComponent from "@/components/HeroComponent";
+import OurDepartmentsSection from "@/components/OurDepartmentsSection";
+import StudentTestimonials from "@/components/StudentTestimonials";
 export default function Home() {
   return (
     <div>
       <HeroComponent />
       <CampusChiefComponent />
       <AboutThapathaliComponent />
+      <OurDepartmentsSection />
+      <CampusImageGallery />
+      <StudentTestimonials />
     </div>
   )
 }
